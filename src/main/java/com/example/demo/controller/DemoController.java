a5172a7c6804c66469a906d6f4e8a2f516ebdccc
tatsuro <ta-kitagawa@isa-school.biz>
2025-06-09 10:08:53 +0900

    ２回目

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -21,7 +21,7 @@ public class DemoController {
 	@GetMapping("/test2")
 	public Test check() {
 	Test test = new Test();
-	test.setName("木村A");
+	test.setName("木村ABC");
 	test.setScore(100);
 	return test;
 	}
